12a542ab7e88e5afc57a9c9b0562b8ff28744fc3
joelvitorniino <joelprofessor13@gmail.com>
2022-07-29 17:52:17 -0300

    Project created

---
+++ src/main/java/com/joelvitorniino/StudyPostgreSqlSpringApplication.java
@@ -0,0 +1,13 @@
+package com.joelvitorniino;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyPostgreSqlSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StudyPostgreSqlSpringApplication.class, args);
+	}
+
+}
